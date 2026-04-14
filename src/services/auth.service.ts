c137f2dbbab6e4e7939c39519bd5eb2d07720125
jfan.dev <jfan.dev@gmail.com>
2026-04-14 10:58:36 -0300

    ✨feat: persist user signup using prisma

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,31 +1,32 @@
 import { randomUUID } from "crypto";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
-const users = [];
+import { prisma } from "../prisma/client.js";
 
 const generateDefaultAvatar = (name) => `https://ui-avatars.com/api/?name=${encodeURIComponent(name)}`;
 
 export async function signup({ name, email, password, birthdate }) {
-  const userExists = users.find(u => u.email === email);
-  const hashedPassword = await bcrypt.hash(password, 10);
-
+  const userExists = await prisma.user.findUnique({
+    where: { email }
+  });
+  
   if (userExists) {
     throw new Error("User already exists");
   }
+  
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newUser = {
-    id: randomUUID(),
-    name,
-    email,
-    password: hashedPassword,
-    birthdate,
-    profileImage: generateDefaultAvatar(name),
-  };
+  const createdUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+      birthdate: new Date(birthdate),
+      profileImage: generateDefaultAvatar(name),
+    },
+  });
 
-  users.push(newUser);
-
-  const { password: _, ...userWithoutPassword } = newUser;
+  const { password: _, ...userWithoutPassword } = createdUser;
 
   return userWithoutPassword;
 }
